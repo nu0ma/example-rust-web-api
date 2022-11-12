711ba2228c17db3145a109dd6950762838502de6
Ryota Ishikawa <igreen.re317v10@gmail.com>
2022-11-12 10:59:34 +0900

    fmt

--- app/user/src/port/user.rs
+++ app/user/src/port/user.rs
@@ -6,7 +6,6 @@ use crate::domain::user::{OrganizationId, User, Users};
 #[async_trait]
 pub trait UserPort {
     async fn get_users_for_id(&self, organization_id: OrganizationId) -> anyhow::Result<Users>;
-
     async fn add_user(&self, name: String, organization_name: String) -> anyhow::Result<User>;
     async fn update_user(&self, id: i32, name: String) -> anyhow::Result<()>;
     async fn delete_user(&self, id: i32) -> anyhow::Result<()>;
